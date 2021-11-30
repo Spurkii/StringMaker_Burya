ec9afcd0453089b700358d41118e386a724724cd
mark burya <mark.burya@studentcentralia.onmicrosoft.com>
2021-11-30 12:30:51 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// Mark Burya
+// IT113
+// NOTES: for reverse, I used some code that my group and I made from our group project from our in class assignment.
+//        also, the capitol 
+//        for the second test, I'm assuming that "one two five three" is supposed to be "one two five four"
+//        because Sunbeam Tiger can't be "one two five three" and can only be "one two five four".
+//        
+// BEHAVIORS NOT IMPLIMENTED AND WHY: I believe I did everything correctly as asked from the assignment aside
+// from test 2 as noted before
+namespace StringMaker_Burya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringManager manager = new StringManager();
+
+            Console.WriteLine("Test 1: " + manager.Reverse("Sunbeam Tiger"));
+            Console.WriteLine("Test 2: " + manager);
+            Console.WriteLine("Test 3: " + manager.Reverse("Sunbeam Tiger", true));
+            Console.WriteLine("Test 4: " + manager.Equals("Sunbeam Tiger"));
+            Console.WriteLine("Test 5: " + manager.Symmetric("ABBA"));
+            Console.WriteLine("Test 6: " + manager.Symmetric("ABA"));
+            Console.WriteLine("Test 7: " + manager.Symmetric("ABba"));
+        }
+    }
+}
---
+++ StringManager.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMaker_Burya
+{
+    class StringManager
+    {
+        private string _s;
+        private Dictionary<int, string> valuePairs = new();
+
+        public string Reverse(string s)
+        {
+            _s = s;
+            Stack<char> cStack = new();
+            string temp = null;
+
+            for (int i = 0; i < _s.Length; i++)
+            {
+                cStack.Push(_s[i]);
+            }
+            for (int i = 0; i < _s.Length; i++)
+            {
+                temp += cStack.Pop();
+            }
+            return temp;
+        }
+        public string Reverse(string s, bool b)
+        {
+            if (!b)
+            {
+                return Reverse(s);
+            }
+            else
+            {
+                _s = s;
+                string temp = null;
+                Stack<char> stack = new();
+                bool[] isCapital = new bool[s.Length];
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i].ToString() == s[i].ToString().ToUpper() && s[i] != ' ')
+                    {
+                        isCapital[i] = true;
+                    }
+                    else
+                    {
+                        isCapital[i] = false;
+                    }
+                    stack.Push(s[i]);
+                }
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (isCapital[i])
+                    {
+                        temp += stack.Pop().ToString().ToUpper();
+                    }
+                    else
+                    {
+                        temp += stack.Pop().ToString().ToLower();
+                    }
+
+                }
+                return temp;
+            }
+
+        }
+        public bool Symmetric(string s)
+        {
+            _s = s;
+            if (s == Reverse(s))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            string t = null;
+            int ASCII = 0;
+            valuePairs.Add(-1,"Negative One");
+            valuePairs.Add(48, "Zero");
+            valuePairs.Add(49, "One");
+            valuePairs.Add(50, "Two");
+            valuePairs.Add(51, "Three");
+            valuePairs.Add(52, "Four");
+            valuePairs.Add(53, "Five");
+            valuePairs.Add(54, "Six");
+            valuePairs.Add(55, "Seven");
+            valuePairs.Add(56, "Eight");
+            valuePairs.Add(57, "Nine");
+
+            foreach (char c in _s)
+            {
+                ASCII += (int)c;
+            }
+
+            if (ASCII == 0)
+            {
+                return valuePairs[-1];
+            }
+            else
+            {
+                string sASCII = ASCII.ToString();
+                for (int i = 0; i < sASCII.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        t = valuePairs[sASCII[i]];
+                    }
+                    else
+                    {
+                        t += " " + valuePairs[sASCII[i]];
+                    }
+                }
+
+                return t;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is string)
+            {
+                return _s == obj.ToString();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
